092e35ff787ccc316543132de0f09aa597a60580
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-01 02:55:45 +0530

    Add GTag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import '../styles/globals.css';
 import { CssBaseline, GeistProvider } from '@geist-ui/core';
 import Layout from '../components/Layout';
+import Script from 'next/script';
 
 const App = ({ Component, pageProps }) => {
   return (
     <GeistProvider>
       <CssBaseline />
       <Layout>
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=UA-49136745-2'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-49136745-2');
+        `}
+        </Script>
         <Component {...pageProps} />
       </Layout>
     </GeistProvider>
