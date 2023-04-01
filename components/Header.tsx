77d3245bed65a2ce7750c890dfff9be1694f95ec
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-01 17:20:44 +0530

    Add Bootstrap

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,52 +1,65 @@
-import { Divider } from '@geist-ui/core';
-import Image from 'next/image';
+import { useSession, signOut } from 'next-auth/react';
 import Link from 'next/link';
 
 export default function Header() {
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
     <nav>
-      <div className='container flex flex-wrap items-center justify-between mx-auto py-3'>
-        <Link href='/' className='flex items-center'>
-          <h2 className='m-0'>Refero</h2>
-        </Link>
+      <div className='container'>
+        <header className='d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3'>
+          <div className='col-md-3 mb-2 mb-md-0'>
+            <Link
+              href='/'
+              className='d-inline-flex link-body-emphasis text-decoration-none'
+            >
+              <h2 className='m-0'>Refero</h2>
+            </Link>
+          </div>
 
-        <div
-          className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
-          id='Header-cta'
-        >
-          <header>
-            <nav>
-              <ul className='flex flex-col m-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-                <li className='m-0'>
-                  <Link href='/users' className='link'>
-                    Users
-                  </Link>
-                </li>
-                <li className='m-0'>
-                  <Link href='/jobs' className='link'>
-                    Jobs
-                  </Link>
-                </li>
-                <li className='m-0'>
-                  <Link href='/resources' className='link'>
-                    Resources
-                  </Link>
-                </li>
-                <li className='m-0'>
-                  <a
-                    href='https://github.com/manassahoo-dev/Refero'
-                    target='_blank'
-                    className='link'
-                  >
-                    Github
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        </div>
+          <ul className='nav col-12 col-md-auto mb-2 justify-content-center mb-md-0'>
+            <li>
+              <Link href='/' className='nav-link px-2 link-secondary'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href='/users' className='nav-link px-4'>
+                Users
+              </Link>
+            </li>
+            <li>
+              <Link href='/jobs' className='nav-link px-4'>
+                Jobs
+              </Link>
+            </li>
+            <li>
+              <Link href='/jobs' className='nav-link px-4'>
+                Jobs
+              </Link>
+            </li>
+          </ul>
+
+          <div className='col-md-3 text-end'>
+            {status === 'authenticated' && (
+              <div>
+                <p>{session.user.name}</p>
+                <button
+                  onClick={() => signOut()}
+                  className='btn btn-outline-primary me-2'
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='/auth/login' className='btn btn-outline-primary me-2'>
+                Login
+              </Link>
+            )}
+          </div>
+        </header>
       </div>
-      <Divider />
     </nav>
   );
 }
