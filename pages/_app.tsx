c55ff897bb1f9d2e388e1eae1de318416880589c
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-03 01:12:10 +0530

    Update Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,22 +8,16 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
     <SessionProvider session={pageProps.session}>
       <Layout>
         <Script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW'
-          crossOrigin='anonymous'
-        />
-        <Script
-          src='https://www.googletagmanager.com/gtag/js?id=UA-49136745-2'
-          strategy='afterInteractive'
-        />
-        <Script id='google-analytics' strategy='afterInteractive'>
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-C0L59QJF4N'
+        ></Script>
+        <Script>
+          {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'UA-49136745-2');
-        `}
+            gtag('config', 'G-C0L59QJF4N');
+          `}
         </Script>
         <Component {...pageProps} />
       </Layout>
