ffb5da73b617eec507692a76076dc10c501be0df
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-05 23:51:56 +0530

    Removed warnings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,14 +18,9 @@ class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
           <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          />
-          <meta
             name='description'
             content='Refero - Connect with Learners and Job Seekers'
           />
-          <title>Refero App</title>
         </Head>
         <body>
           <Main />
