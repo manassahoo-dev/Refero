20da706d279e5a9376e684f6a2ad09aaf37d47e2
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-05 23:51:12 +0530

    optimized version

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
-  render() {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
     return (
       <Html lang='en'>
         <Head>
