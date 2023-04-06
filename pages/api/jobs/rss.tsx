7d15e4a8250c2f53b80487f90ec4e4e3c4977e2c
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-04-06 23:33:51 +0530

    Refactor

--- pages/api/jobs/rss.tsx
+++ pages/api/jobs/rss.tsx
@@ -1,18 +1,15 @@
-export default async function handler(req, res) {
-  const urls = [
-    'https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss',
-    'https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss',
-    'https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss',
-  ];
+import { NextApiHandler } from 'next';
 
-  const promises = urls.map((url) => fetch(url));
+const urls = [
+  'https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss',
+  'https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss',
+  'https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss',
+];
 
-  try {
-    const responses = await Promise.all(promises);
-    const data = await Promise.all(responses.map((res) => res.text()));
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error fetching data' });
-  }
-}
+const handler: NextApiHandler = async (req, res) => {
+  const responses = await Promise.all(urls.map((url) => fetch(url)));
+  const data = await Promise.all(responses.map((res) => res.text()));
+  res.status(200).json(data);
+};
+
+export default handler;
